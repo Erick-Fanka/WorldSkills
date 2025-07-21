9749c790394356da42894b64616da6c20c331b78
Erick Fanka <erickfanka@gmail.com>
2025-07-21 19:10:54 +0000

    Adicionando novamente scripts corrigidos após merge

---
+++ arquivos.sh/webserver.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script de inicialização para EC2 com Apache, PHP e conteúdo de bucket S3.
+# Nome do arquivo: World Skills/arquivos.sh/userdata-webserver
+# Autor: Erick Fanka
+# Data: julho de 2025
+
+# Atualiza pacotes e instala Apache, PHP e unzip
+sudo yum update -y
+sudo yum install -y httpd php unzip aws-cli
+
+# Inicia e habilita o Apache
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+# Verifica se o bucket está acessível
+aws s3 ls s3://<name-bucket>
+
+# Baixa o arquivo zip do site
+aws s3 cp s3://<name-bucket>/<name-file> /home/ec2-user/
+
+# Extrai o conteúdo para o diretório do Apache
+sudo unzip -o /home/ec2-user/<name-file> -d /var/www/html/
+
+# Move arquivos de /<name-file> para /var/www/html e remove o diretório
+sudo mv /var/www/html/<name-file>/* /var/www/html/
+sudo rm -rf /var/www/html/<name-file>
+
+# Ajusta permissões
+sudo chown -R apache:apache /var/www/html
+sudo find /var/www/html -type d -exec chmod 755 {} \;
+sudo find /var/www/html -type f -exec chmod 644 {} \;
+
+# Reinicia o Apache
+sudo systemctl restart httpd
